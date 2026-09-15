9857c4f7b7d06b57a200eb6fcdf1e29cfb939fdb
Lana Schmitt <lanaschtt@gmail.com>
2026-09-15 15:21:43 +0200

    ajout du script ResetBalle qui téléporte la balle à son point de départ lorsqu'elle touche la bordure du bas

---
+++ Assets/Script/ResetBalle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ResetBalle : MonoBehaviour
+{
+    public Transform pointDeDepart;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Le script vérifie maintenant que seul l'objet nommé "la_bille" est téléporté
+        if (other.gameObject.name == "la_bille")
+        {
+            // Téléporte la bille au point de départ
+            other.transform.position = pointDeDepart.position;
+
+            // Annule la vitesse et la rotation de la bille pour qu'elle reparte proprement
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
