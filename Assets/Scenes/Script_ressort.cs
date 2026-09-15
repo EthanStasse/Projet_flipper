f7b0f6e5fc90f1ff9370b7e4bb58813cf35efd99
Lana Schmitt <lanaschtt@gmail.com>
2026-09-15 17:30:52 +0200

    résolution du conflit

--- Assets/Scenes/Script_ressort.cs
+++ Assets/Scenes/Script_ressort.cs
@@ -44,12 +44,9 @@ public class Script_ressort : MonoBehaviour
 
     void Update()
     {
-        // Accès via la classe Pinball_input, l'action map "inputs" et l'action "joystick" (<Gamepad>/dpad/down)
-<<<<<<< HEAD
-        if (controls.inputs.start.WasPressedThisFrame() || controls.inputs.start1.WasPressedThisFrame() && !enMouvement)
-=======
+
         if (controls.inputs.start.WasPressedThisFrame() || Keyboard.current.spaceKey.wasPressedThisFrame && !enMouvement)
->>>>>>> 44275fac72c44d73d8e5f9e7b698cd2908adf971
+
         {
             enMouvement = true;
             versArrivee = true;
